fb44a66f9414d96f0f3df3bd0a33a1421c88bc2f
Denis-us <75166604+Denis-us@users.noreply.github.com>
2021-04-23 20:56:40 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,22 @@
-const categoriesItem = document.querySelector('li.item')
-console.log(categoriesItem)
+const allCategories = document.querySelector('#categories');
+console.log(`В списке ${allCategories.children.length} категории`)
+
+
+const firstCategoryName = document.querySelector('#categories li.item');
+console.log(`Категория: ${firstCategoryName.firstElementChild.textContent}`);
+
+const elementsOfFirstCategory = firstCategoryName.lastElementChild;
+
+const arrayElementsOfFirstCategory = elementsOfFirstCategory.querySelectorAll('#categories li.item ul li');
+console.log(`Количество элементов: ${arrayElementsOfFirstCategory.length}`);
+
+
+
+// const categoryName = document.querySelector('ul#categories');
+
+// const categoryName = document.querySelector('.item');
+// console.log(categoryName);
+// secondCategoryName = categoryName[1].firstElementChild.textContent;
+// console.log(secondCategoryName);
+
+
