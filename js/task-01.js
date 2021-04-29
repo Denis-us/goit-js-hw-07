3c74d40e0b7caed6b34ecf05060f7f743aeb86b0
Denis-us <75166604+Denis-us@users.noreply.github.com>
2021-04-29 22:13:46 +0300

    исправил 1 задание

--- js/task-01.js
+++ js/task-01.js
@@ -1,16 +1,10 @@
-const allCategories = document.querySelector('#categories');
-console.log(`В списке ${allCategories.children.length} категории`)
+const categoriesEl = document.querySelectorAll('.item');
+console.log(`В списке ${categoriesEl.length} категории`)
 
-const categoryName = document.querySelectorAll('.item');
-const firstCategory = categoryName[0].querySelectorAll('li');
-const secondCategory = categoryName[1].querySelectorAll('li');
-const thirdCategory = categoryName[2].querySelectorAll('li');
-console.log(`Категория: ${categoryName[0].firstElementChild.textContent}`);
-console.log(`Количество элементов: ${firstCategory.length}`);
-console.log(`Категория: ${categoryName[1].firstElementChild.textContent}`);
-console.log(`Количество элементов: ${secondCategory.length}`);
-console.log(`Категория: ${categoryName[2].firstElementChild.textContent}`);
-console.log(`Количество элементов: ${thirdCategory.length}`);
+categoriesEl.forEach(item => {
+    console.log(`Категория: ${item.querySelector('h2').textContent}`);
+    console.log(`Количество элементов: ${item.querySelector('ul').children.length}`);
+})
 
 
 
